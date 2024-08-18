422b5afbfe78699a9309694bbaa273f657e0a634
Phillip <phillip@growento.com>
2024-08-18 14:28:41 +0200

    deleted existing backend and add new folder

---
+++ frontend/src/components/FloatingStaff_02.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './floatingStaff.css';
+
+function FloatingStaff_02() {
+  return (
+    <div className="container">
+      <div className="avatar">
+        <a href="https://codepen.io/">
+          <img src="https://media.licdn.com/dms/image/D4E03AQHrbA3hT6UlLA/profile-displayphoto-shrink_800_800/0/1723686486084?e=1729123200&v=beta&t=cn7SlNtSPjT4Y1e2fiXZIEXxYKPN97aL90tPGfbAGiE" alt="Georgi" />
+        </a>
+      </div>
+      <div className="content">
+        <h1>Georgi Slavov</h1>
+        <p>Graphic & UX Designer</p>
+        <p>https://www.linkedin.com/in/georgi-slavov/</p>
+      </div>
+    </div>
+  );
+}
+
+export default FloatingStaff_02;
